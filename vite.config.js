a28e09b60d7557b79330a3c5eb75bb1a4845ef09
drignet <odibehenry@gmail.com>
2024-05-13 02:15:36 +0100

    update

---
+++ vite.config.js
@@ -0,0 +1,41 @@
+// vite.config.js
+import { defineConfig } from "vite";
+import { html } from "vite-plugin-html";
+
+export default defineConfig({
+  plugins: [
+    html({
+      inject: {
+        injectTo: "body",
+        entryOrder: "ascending",
+      },
+      minify: true,
+      hash: true,
+      filename: "index.html",
+      title: "Your App Title",
+      chunks: ["main"], // Main entry point (main.js)
+    }),
+    html({
+      inject: {
+        injectTo: "body",
+        entryOrder: "ascending",
+      },
+      minify: true,
+      hash: true,
+      filename: "air.html",
+      title: "Your Air Page Title",
+      chunks: ["main"], // Main entry point (main.js)
+    }),
+    html({
+      inject: {
+        injectTo: "body",
+        entryOrder: "ascending",
+      },
+      minify: true,
+      hash: true,
+      filename: "sea.html",
+      title: "Your Sea Page Title",
+      chunks: ["main"], // Main entry point (main.js)
+    }),
+  ],
+});
