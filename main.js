690b3b46bf9d55fb0f89ba864a4d8c0e6e69052a
drignet <odibehenry@gmail.com>
2024-05-20 01:48:00 +0100

    update

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const weightUnit = document.querySelector("#weight");
 const results = document.querySelectorAll(".results");
 const amountAir = document.querySelector("#amount-value-air");
 const nairaRateAir = document.querySelector("#naira-rate-air");
+const airClearingFee = document.querySelector("#air-clearing-fee");
 const airSubmitBtn = document.querySelector("#air-submit");
 const seaSubmitBtn = document.querySelector("#sea-submit");
 const cbmUnit = document.querySelector("#cbm");
@@ -12,6 +13,7 @@ const height = document.querySelector("#height");
 const cbmValue = document.querySelector(".cbm");
 const nairaRateSea = document.querySelector("#naira-rate-sea");
 const amountSea = document.querySelector("#amount-value-sea");
+const seaClearingFee = document.querySelector("#sea-clearing-fee");
 const shipPage = document.querySelector(".ship");
 
 const airPageBtn = document.querySelector("#airPage");
@@ -29,34 +31,36 @@ seaPageBtn?.addEventListener("click", () => {
 
 airSubmitBtn?.addEventListener("click", () => {
   weightUnit.value === "kg"
-    ? (results[0].children[1].textContent = (
+    ? (results[0].children[1].textContent = ((
         +weightInput.value *
         +amountAir.value *
         +nairaRateAir.value
-      ).toFixed(2))
-    : (results[0].children[1].textContent = (
+    ) + (+airClearingFee.value * +weightInput.value)).toFixed(2))
+    : (results[0].children[1].textContent = ((
         (+weightInput.value / 1000) *
         +amountAir.value *
         +nairaRateAir.value
-      ).toFixed(2));
+      ) + (+airClearingFee.value * (+weightInput.value / 1000))).toFixed(2));
 });
 
 seaSubmitBtn?.addEventListener("click", () => {
   let volume = +length.value * +width.value * +height.value;
   if (cbmUnit.value === "cm") {
-    cbmValue.textContent = (volume / 1000000).toFixed(2);
-    results[0].children[1].textContent = (
+    let cbmCM = volume / 1000000;
+    cbmValue.textContent = cbmCM.toFixed(2);
+    results[0].children[1].textContent = ((
       (volume / 1000000) *
       +amountSea.value *
       +nairaRateSea.value
-    ).toFixed(2);
+    ) + (cbmCM * +seaClearingFee.value)).toFixed(2);
   } else {
-    cbmValue.textContent = volume / 1000000000;
-    results[0].children[1].textContent = (
+    let cbmMM = volume / 1000000000;
+    cbmValue.textContent = cbmMM;
+    results[0].children[1].textContent = ((
       (volume / 1000000000) *
       +amountSea.value *
       +nairaRateSea.value
-    ).toFixed(2);
+    ) + (cbmMM * +seaClearingFee.value)).toFixed(2);
   }
 });
 
